19c368b7177c944ae81cdb9aa5f2160255b54a80
studynovabot <thakurranveersingh505@gmail.com>
2025-04-27 20:49:39 +0530

    update

--- .next/static/chunks/app/layout-2639486bcbb4f1af.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4325:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9324,23)),Promise.resolve().then(l.t.bind(l,7725,23))},7725:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[628,441,684,358],()=>s(4325)),_N_E=e.O()}]);
